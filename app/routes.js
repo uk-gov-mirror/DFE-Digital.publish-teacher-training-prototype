a0f8bd087162b98132b5171056e421da962fd537
Jesse Yuen <hello@jesseyuen.com>
2020-08-03 17:29:17 +0100

    Iterations on the flow

--- app/routes.js
+++ app/routes.js
@@ -658,7 +658,7 @@ router.post('/users/manage-courses-1', (req, res) => {
 })
 
 router.post('/users/whose-courses-1', (req, res) => {
-  res.redirect('/users/manage-users-1')
+  res.redirect('/users/next-org')
 })
 
 router.post('/users/manage-users-1', (req, res) => {
